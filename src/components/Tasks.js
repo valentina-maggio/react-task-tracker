19478d35abbf071bec81c6128b82bb659bef6012
Valentina <maggio.vale@gmail.com>
2022-04-24 19:31:58 +0100

    Move tasks state to App.js

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,27 +1,4 @@
-import { useState } from 'react';
-
-const Tasks = () => {
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: 'Doctors appointment',
-      day: '5 May at 2:30pm',
-      reminder: true,
-    },
-    {
-      id: 2,
-      text: 'Meeting at school',
-      day: '6 May at 1:30pm',
-      reminder: true,
-    },
-    {
-      id: 3,
-      text: 'Food shopping',
-      day: '5 May at 2:30pm',
-      reminder: false,
-    }
-  ])
-
+const Tasks = ({ tasks }) => {
   return (
     <>
       {tasks.map((task) => (
