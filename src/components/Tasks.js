173ff993b3e2ef641c4a6bd5e85349feb9c2cd29
Valentina <maggio.vale@gmail.com>
2022-04-24 19:27:12 +0100

    Add useState hook to make tasks part of the state

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,25 +1,27 @@
-const tasks = [
-  {
-    id: 1,
-    text: 'Doctors appointment',
-    day: '5 May at 2:30pm',
-    reminder: true,
-  },
-  {
-    id: 2,
-    text: 'Meeting at school',
-    day: '6 May at 1:30pm',
-    reminder: true,
-  },
-  {
-    id: 3,
-    text: 'Food shopping',
-    day: '5 May at 2:30pm',
-    reminder: false,
-  }
-]
+import { useState } from 'react';
 
 const Tasks = () => {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Doctors appointment',
+      day: '5 May at 2:30pm',
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: 'Meeting at school',
+      day: '6 May at 1:30pm',
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: 'Food shopping',
+      day: '5 May at 2:30pm',
+      reminder: false,
+    }
+  ])
+
   return (
     <>
       {tasks.map((task) => (
