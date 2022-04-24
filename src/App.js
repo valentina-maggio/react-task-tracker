bbc035934c0195fa3be0ec2621342e9afff1b408
Valentina <maggio.vale@gmail.com>
2022-04-24 20:26:26 +0100

    Add function to delete task

--- src/App.js
+++ src/App.js
@@ -24,10 +24,15 @@ function App() {
     }
   ])
 
+  // Delete Task
+  const deleteTask = (id) => {
+    console.log('delete', id);
+  }
+
   return (
     <div className='container'>
       <Header />
-      <Tasks tasks={tasks} />
+      <Tasks tasks={tasks} onDelete={deleteTask}/>
     </div>
   );
 }
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,11 +1,16 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
-const Task = ({ task }) => {
+const Task = ({ task, onDelete }) => {
   return (
     <div className='task'>
       <h3>
-        {task.text} <FontAwesomeIcon icon={faTimes} style={{color: 'red', cursor: 'pointer'}} />
+        {task.text} 
+        <FontAwesomeIcon 
+        icon={faTimes} 
+        style={{color: 'red', cursor: 'pointer'}} 
+        onClick={() => onDelete(task.id)}
+        />
       </h3>
       <p>{task.day}</p>
     </div>
