2378c85bda7cf60db7ddd5d8de70c4708453edcd
Valentina <maggio.vale@gmail.com>
2022-04-24 22:27:42 +0100

    Building a mock backend with json server
    - dependencies installation to run json server

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,9 +6,9 @@ const Header = ({ title, onAdd, showAdd }) => {
     <header className='header'>
       <h1>{title}</h1>
       <Button 
-      color={showAdd ? 'red' : 'green'} 
-      text={showAdd ? 'Close' : 'Add'} 
-      onClick={onAdd}/>
+        color={showAdd ? 'red' : 'green'} 
+        text={showAdd ? 'Close' : 'Add'} 
+        onClick={onAdd}/>
     </header>
   )
 }
