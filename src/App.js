6d0ea646a8d4025d1a2bc322b94718cd82ae269e
Valentina <maggio.vale@gmail.com>
2022-04-24 22:32:39 +0100

    Move tasks state to db.json

--- src/App.js
+++ src/App.js
@@ -5,26 +5,7 @@ import AddTask from './components/AddTask';
 
 function App() {
   const [showAddTask, setShowAddTask] = useState(true);
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: 'Doctors appointment',
-      day: '5 May at 2:30pm',
-      reminder: true,
-    },
-    {
-      id: 2,
-      text: 'Meeting at school',
-      day: '6 May at 1:30pm',
-      reminder: true,
-    },
-    {
-      id: 3,
-      text: 'Food shopping',
-      day: '5 May at 2:30pm',
-      reminder: false,
-    }
-  ])
+  const [tasks, setTasks] = useState([]);
 
   // Add Task
 
