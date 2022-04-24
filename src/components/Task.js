1780b15c004c855c893c29cf9ed0e83f547afe6d
Valentina <maggio.vale@gmail.com>
2022-04-24 20:15:54 +0100

    Add icons package to use icons in Task component

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,7 +1,12 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
 const Task = ({ task }) => {
   return (
     <div className='task'>
-      <h3>{task.text}</h3>
+      <h3>
+        {task.text} <FontAwesomeIcon icon={faTimes} style={{color: 'red', cursor: 'pointer'}} />
+      </h3>
       <p>{task.day}</p>
     </div>
   )
