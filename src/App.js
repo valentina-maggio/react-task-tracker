dfc988ecd28a098c33ae2ee3f1fd8342ec3947e8
Valentina <maggio.vale@gmail.com>
2022-04-24 20:31:14 +0100

    Logic for deleting tasks and displaying message

--- src/App.js
+++ src/App.js
@@ -26,13 +26,17 @@ function App() {
 
   // Delete Task
   const deleteTask = (id) => {
-    console.log('delete', id);
+    setTasks(tasks.filter((task) => task.id !== id));
   }
 
   return (
     <div className='container'>
       <Header />
-      <Tasks tasks={tasks} onDelete={deleteTask}/>
+      {tasks.length > 0 ? (
+      <Tasks tasks={tasks} onDelete={deleteTask} />
+      ) : (
+        'No tasks to show'
+      )}
     </div>
   );
 }
