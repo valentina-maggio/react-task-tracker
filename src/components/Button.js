104925344359e28d591fd967e79fbfe5526b4ac3
Valentina <maggio.vale@gmail.com>
2022-04-24 19:19:16 +0100

    Embed Button component in Header
    - add prop types to Button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,25 @@
-const Button = () => {
+import PropTypes from 'prop-types'
+
+const Button = ({ color, text, onClick }) => {
   return (
-    <div>
-      
-    </div>
+      <button 
+      onClick={onClick} 
+      style={{ backgroundColor: color}}
+      className='btn'
+      >
+        {text}
+      </button>
   )
 }
 
-export default Button
+Button.defaultProps = {
+  color: 'steelblue'
+}
 
+Button.propTypes = {
+  text: PropTypes.string,
+  color: PropTypes.string,
+  onClick: PropTypes.func,
+}
+
+export default Button
